34726b9cd46e7571d43b819e32e5eeb8af78c3ab
Zohaib ur Rehman <mianzhb@gmail.com>
2023-06-13 14:28:34 +0500

    cors updates

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT);
 }
